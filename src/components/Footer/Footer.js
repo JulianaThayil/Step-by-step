2e9c27a1efe7ef59e88440f14c399526951ac9e3
flinn <flinn100@gmail.com>
2020-04-04 21:31:42 +0530

    footer component update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,11 +4,11 @@ import classes from '../Footer/Footer.module.css'
 function footer() {
     return(
         <div className={classes.footOverall}> 
-                <h1 className={classes.h1}>@Copyrights2020</h1>
-                <ul className={classes.footInfo}>
-                    <li>About us</li>
-                    <li>Contact us</li>
-                </ul>
+                <h1 className={classes.h1}>Copyrights@2020</h1>
+                <div className={classes.footInfo}>
+                    <p href="#">About us</p>
+                    <p href="#">Contact us</p>
+                </div>
                 <h3>Made with Love in Goa.</h3>
             </div>
     );
