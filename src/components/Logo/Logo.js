5cc85eb3cd6f9affaf9186afe75eda9a38e161ee
Swizel <swizelmonteiro012@gmail.com>
2020-04-20 19:40:41 +0530

    logo added

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -4,8 +4,38 @@ const logo =()=>
 {
     return(
           <div className={classes.logo} >
-          <img  width="5%" height="5%" src="/logo.png"></img>
-           <b> <h4 className={classes.h3}>Step-by-Step </h4></b>
+        <div className={classes.container}>
+<div className={classes.heart}>
+  <div className={classes.square1}></div>
+    <div className={classes.circle1}></div>
+      <div className={classes.circle2}></div>
+</div>
+<div className={classes.spoon}>
+  <div className={classes.stick}></div>
+  <div className={classes.circle3}></div>
+</div>
+<div className={classes.fork}>
+  <div className={classes.stick2}></div>
+  <div className={classes.circle4}></div>
+   <div className={classes.square2}></div>
+  <div className={classes.square3}></div>
+</div>
+  <div className={classes.knife}>
+    <div className={classes.stick3}></div>
+    <div className={classes.square4}></div>
+    <div className={classes.circle5}></div>
+  </div>
+  <div className={classes.steps}>
+    <div className={classes.square5}> </div>
+     <div className={classes.square6}></div>
+      <div className={classes.square7}> </div>
+  </div>
+  <div className={classes.name}>
+  <div className={classes.name1}>STEP</div>
+  <div className={classes.name2}>BY</div>
+  <div className={classes.name3}>STEP</div>
+  </div>
+</div>
         </div>
     )
 }
