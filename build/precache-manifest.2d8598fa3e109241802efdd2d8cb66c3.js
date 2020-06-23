5217fc7c60010e3abbfc6cee4278694531af8a60
juliana thayil <julianathayil@gmail.com>
2020-06-23 15:18:44 +0530

    build

---
+++ build/precache-manifest.2d8598fa3e109241802efdd2d8cb66c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df261dd12fe057261808fa586d9846c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff5d685b48e4e2e6ec5c",
+    "url": "/static/css/main.760bd53e.chunk.css"
+  },
+  {
+    "revision": "5bc1a9d534d9754b341d",
+    "url": "/static/js/2.42cf8175.chunk.js"
+  },
+  {
+    "revision": "923445ec5bd7e177c758a90d9a1e52b2",
+    "url": "/static/js/2.42cf8175.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff5d685b48e4e2e6ec5c",
+    "url": "/static/js/main.32cae465.chunk.js"
+  },
+  {
+    "revision": "cf0904f571a6fac9672a",
+    "url": "/static/js/runtime-main.fac74e0e.js"
+  }
+]);
